782efd468d807931328bbf1bda6df0da53b50453
Stefan Unković <64104113+Mergevos@users.noreply.github.com>
2024-04-29 01:25:39 +0200

    Update checking for wiki article

--- bot_github.py
+++ bot_github.py
@@ -3,17 +3,18 @@ import git
 
 import discord
 
-def wiki_article(article: str) -> None:
-    url = f"https://github.com/pulsepm/pulse/wiki/{str(article)}"
-    response = requests.get(url)
-    print(url)
-    if response.status_code == 200:
-        print(f"The wiki page '{article}' exists.")
-    elif response.status_code == 404:
-        print(f"The wiki page '{article}' does not exist.")
-    else:
-        print(f"Failed to check the existence of the wiki page. Status code: {response.status_code}")
+def wiki_article(article: str) -> bool:
+  """Checks if a GitHub wiki page exists using HEAD request.
 
+  Args:
+      url (str): The URL of the GitHub wiki page.
+
+  Returns:
+      bool: True if the page exists, False otherwise.
+  """
+  headers = {'Accept': 'application/json'} 
+  response = requests.head(f"https://github.com/pulsepm/pulse/wiki/{article}", headers=headers)
+  return response.status_code == 200
 
 def wiki_embed():
     embed = discord.Embed(
